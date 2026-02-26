3c9a386164618e3f5ae706852bb5f4f48be48173
Shailendra Yadav <shailendrayadaw2004@gmail.com>
2026-02-26 12:24:33 +0530

    nwe

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Products from './App'
-import DevPortfolio from './apps'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
